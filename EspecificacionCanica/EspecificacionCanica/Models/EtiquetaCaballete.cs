7de7560fdb384969c3451e7a589e2963c5e7f9b5
Github2715 <mirismarquez@gmail.com>
2022-10-31 16:35:11 -0600

    Add caballete

---
+++ EspecificacionCanica/EspecificacionCanica/Models/EtiquetaCaballete.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace EspecificacionCanica.Models
+{
+    public class EtiquetaCaballete
+    {
+        [DisplayName("Código de Barras (Etiqueta):")]
+        public string CodBarras { get; set; }
+        [DisplayName("Código SAP Etiqueta:")]
+        public string CodSAPEtiqueta { get; set; }
+        public string descripcion { get; set; }
+        public List<CanicaCaballete> canicas { get; set; }
+    }
+
+    public class CanicaCaballete
+    {
+        public string nombre { get; set; }
+    }
+}
